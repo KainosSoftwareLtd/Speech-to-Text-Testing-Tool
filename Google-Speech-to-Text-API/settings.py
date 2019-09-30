cffb6d7fd5e58ada4b171545a5bee17425de5ee9
Matthew Leyburn <mattleyburn@hotmail.co.uk>
2019-09-30 11:56:52 +0100

    add sample results files to Microsoft

--- Google-Speech-to-Text-API/settings.py
+++ Google-Speech-to-Text-API/settings.py
@@ -4,10 +4,10 @@ REPLACE THESE VARIABLES WITH YOUR OWN BEFORE RUNNING SCRIPT
 
 PATH_TO_GOOGLE_CREDENTIALS_JSON = "voice-to-text-testing-75b6d2a7bac5.json"
 
-HYP_PATH = 'results/hyp.txt'
-REF_PATH = 'results/ref.txt'
-RESULTS_TABLE_PATH = 'results/table.txt'
-RESULTS_CSV_PATH = 'results/results.csv'
+HYP_PATH = 'results/sample/hyp.txt'
+REF_PATH = 'results/sample/ref.txt'
+RESULTS_TABLE_PATH = 'results/sample/table.txt'
+RESULTS_CSV_PATH = 'results/sample/results.csv'
 ALL_TRANCRIPTIONS_PATH = 'results/allTranscriptions.txt'
 
 AUDIO_FORMAT = 'FLAC'
