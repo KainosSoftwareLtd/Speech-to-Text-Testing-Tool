50c839c5df792893edef3e99bd2b5cbb3440f331
Matthew Leyburn <mattleyburn@hotmail.co.uk>
2019-09-30 14:35:25 +0100

    update Google settings filepaths

--- Google-Speech-to-Text-API/settings.py
+++ Google-Speech-to-Text-API/settings.py
@@ -2,12 +2,12 @@
 REPLACE THESE VARIABLES WITH YOUR OWN BEFORE RUNNING SCRIPT
 """
 
-PATH_TO_GOOGLE_CREDENTIALS_JSON = "voice-to-text-testing-75b6d2a7bac5.json"
+PATH_TO_GOOGLE_CREDENTIALS_JSON = "<YourGoogleCredentialsJson>.json"
 
-HYP_PATH = 'results/sample/hyp.txt'
-REF_PATH = 'results/sample/ref.txt'
-RESULTS_TABLE_PATH = 'results/sample/table.txt'
-RESULTS_CSV_PATH = 'results/sample/results.csv'
+HYP_PATH = 'results/hyp.txt'
+REF_PATH = 'results/ref.txt'
+RESULTS_TABLE_PATH = 'results/table.txt'
+RESULTS_CSV_PATH = 'results/results.csv'
 ALL_TRANCRIPTIONS_PATH = 'results/allTranscriptions.txt'
 
 AUDIO_FORMAT = 'FLAC'
